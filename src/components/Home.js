195d967d8c7bb76b545058712ae6fa824ff846e9
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-23 20:34:58 +0530

    Basic functionality added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [data, setData] = useState(null);
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v4/launchpads")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log("DATA =", data);
+        for (let i = 0; i < 3; i++) {
+          if (data.launches[i])
+            setLaunches((prevVals) => [...prevVals, data.launches[i]]);
+          console.log("LAUNCHESZ:", launches);
+        }
+      })
+      .catch((error) => console.log("ERROR!", error));
+  }, []);
+
+  //name = item.name
+  //status = item.status
+  //image =item.images.large[0]
+  //details...
+  //item.launch_attempts
+  //item.launch_successes
+  //item.region
+
+  //...top 3 launches
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {data && (
+        <div>
+          {data.map((item) => (
+            <div>
+              <h1>{item.name}</h1>
+              {/* {console.log(item.images.large[0])} */}
+              <img
+                src={item.images.large[0]}
+                alt="img"
+                height={400}
+                width={300}
+              ></img>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
