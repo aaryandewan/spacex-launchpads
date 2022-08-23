c64a3bcbb98d5b9ae0633c6658fe965ae21c1195
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-23 19:57:15 +0530

    Routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { lazy, Suspense } from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Home = lazy(()=> import("./components/Home"))
+const Launchpad = lazy(()=> import("./components/Launchpad"))
+const NotFound = lazy(()=> import("./components/NotFound"))
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading</p>}></Suspense>
+      <Routes>
+      <Route path="/" element={<Home />} ></Route>
+      <Route path="/launchpad/:id" element={<Launchpad />} ></Route>
+      <Route path="*" element={<NotFound />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
